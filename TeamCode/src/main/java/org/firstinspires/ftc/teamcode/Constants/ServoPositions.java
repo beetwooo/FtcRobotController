eaf15e9b1898756df9282b243b2a3de153dd782e
b2 <betwo.kms@gmail.com>
2026-07-25 00:40:34 +0900

    Update 5

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants/ServoPositions.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.Constants;
+
+public class ServoPositions {
+    public static double servo_eat_down = 0.5;
+    public static double servo_eat_up = 0.7;
+
+
+    public static double servo_shoot_block = 0.495;
+    public static double servo_shoot_go = 0.35;
+
+    public static double servo_hood_min = 1;
+    public static double servo_hood_max = 0.21;
+
+    public static double servo_leftlift_down = 0.15;
+    public static double Servo_leftlift_up = 0.5;
+
+    public static double servo_rightlift_down = 0.85;
+    public static double servo_rightlift_up = 0.45;
+
+}
